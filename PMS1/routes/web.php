55ef856717b8dd95410ceb9d661d636613f3bdcf
june-milan <justinemaem03@gmail.com>
2024-08-21 20:14:04 -0700

    Patient Records added to Medical Staff Dashboard

--- PMS1/routes/web.php
+++ PMS1/routes/web.php
@@ -3,15 +3,22 @@
 use App\Http\Controllers\PatientController;
 use Illuminate\Support\Facades\Route;
 
+// Define a route for the home page
 Route::get('/', function () {
-    return view('patient_registration');
+    return view('admin_med.index'); // Render the 'admin_med.index' view
 });
 
+// Define a route for the records page
+Route::get('records', function () {
+    return view('admin_med.records'); // Render the 'admin_med.records' view
+});
+
+// Group routes that use the PatientController
 Route::controller(PatientController::class)->group(function() {
-    Route::get('/index', 'index');
-    Route::get('/create', 'create');
-    Route::post('/store', 'store');
+    Route::get('/index', 'index'); // Route to the index method
+    Route::get('/create', 'create'); // Route to the create method
+    Route::post('/store', 'store'); // Route to the store method
 
+    // Route to show a specific patient by ID
     Route::get('/patients/{patient_id}', 'show')->name('patients.show');
-
 });
